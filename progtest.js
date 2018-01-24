2503db8537f459dfc3b91b93f869a4f75b983a8e
Wouter <wouter@MacBook-Pro-van-Wouter.local>
2018-01-24 23:06:12 +0100

    jup

--- progtest.js
+++ progtest.js
@@ -1,10 +1,16 @@
+var test;
+test = "hello"
+console.log(test);
 var mathsteps = require('mathsteps');
 
-var steps = mathsteps.simplifyExpression('2x + 2x + x + x');
+var steps = mathsteps.simplifyExpression('2x + 2x^(2+1) + x + x');
 
 steps.forEach(step => {
-	console.log("before change: " + step.oldNode.toString());   // before change: 2 x + 2 x + x + x
-	console.log("change: " + step.changeType);                  // change: ADD_POLYNOMIAL_TERMS
-	console.log("after change: " + step.newNode.toString());    // after change: 6 x
-	console.log("# of substeps: " + step.substeps.length);      // # of substeps: 3
+	console.log("before change: " + step.oldNode.toString()); 
+	console.log("change: " + step.changeType); 
+	console.log("after change: " + step.newNode.toString());
+	console.log("# of substeps: " + step.substeps.length); 
 });
+
+console.log(3+4);
+
